b790dc0f7c9863f5c4390393a3fa4cb0b026bf04
Prakher Prashank <43149558+terabo090@users.noreply.github.com>
2019-07-31 14:21:02 -0700

    Add files via upload

---
+++ notes-app/notes.js
@@ -0,0 +1,76 @@
+const fs=require('fs');
+const chalk = require('chalk')
+
+const getNotes=()=>{
+    return "Your notes are here..."
+}
+
+const addNote=(title,body)=>{
+    const notes=loadNotes();
+    const duplicateNote =notes.find((note)=>
+         note.title===title );
+
+    if(!duplicateNote){
+    notes.push({
+        title:title,
+        body:body
+    });
+    saveNotes(notes);
+    console.log(chalk`{green.bold New note added!}`);
+}else{ss
+    console.log(chalk`red.bold Note title taken!`);
+}
+}
+
+const readNote=(title)=>{
+    const notes=loadNotes();
+    const found_note=notes.find(note=> note.title===title);
+    if(found_note)
+        console.log(chalk`{blue ${found_note.title} } \n{yellow ${found_note.body}}`)
+    else
+        console.log(chalk`{red Node not found}`);
+}
+
+const removeNote= function(title){
+    const notes=loadNotes();
+    const upd_notes=notes.filter((note)=>{
+        return note.title !== title
+    });
+    if(notes.length>upd_notes.length)
+        console.log(chalk`{green.bold.inverse Node removed}`);
+    else
+        console.log(chalk`{red.bold.inverse Node not found}`);
+    saveNotes(upd_notes);
+}
+
+const listNotes=()=>{
+    console.log(chalk`{cyan  Your Notes}`)
+    const notes=loadNotes();
+    notes.forEach(note => {
+        console.log(note.title);
+    });
+}
+
+const saveNotes=(notes)=>{
+    const dataJSON=JSON.stringify(notes);
+    fs.writeFileSync('notes.json',dataJSON);
+}
+
+const loadNotes=()=>{
+   try{
+    const dataBuffer= fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON)
+   }
+   catch(e){
+       return [];
+   }_
+}
+
+module.exports= {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote:removeNote,
+    listNotes: listNotes,
+    readNote: readNote
+};
